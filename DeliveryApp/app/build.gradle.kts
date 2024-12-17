1205c8259d6b9f1fa6b3a4848af0192a7a39af74
Yamada Koki <90810018+k20rs141@users.noreply.github.com>
2024-12-17 17:39:11 +0900

    Add: API通信に必要なライブラリとViewModelのライブラリの追加

--- DeliveryApp/app/build.gradle.kts
+++ DeliveryApp/app/build.gradle.kts
@@ -62,4 +62,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // Retrofit compose
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2.converter.gson)
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.livedata)
 }
