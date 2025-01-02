20dc469b85e747c705cb3d2147f9ad637d89b551
Yamada Koki <90810018+k20rs141@users.noreply.github.com>
2025-01-03 00:25:49 +0900

    Update: minSdkの変更
    実機を動かすためにグレードダウン

--- DeliveryApp/app/build.gradle.kts
+++ DeliveryApp/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.deliveryapp"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
