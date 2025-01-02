8f988cdf8401c354374652baaf862e9ac1c12779
Yamada Koki <90810018+k20rs141@users.noreply.github.com>
2025-01-03 00:34:26 +0900

    Update: Builderのclassをobjectに変更

--- DeliveryApp/app/src/main/java/com/example/deliveryapp/data/OhtomiAppContainer.kt
+++ DeliveryApp/app/src/main/java/com/example/deliveryapp/data/OhtomiAppContainer.kt
@@ -1,38 +1,99 @@
 package com.example.deliveryapp.data
 
-import com.example.deliveryapp.model.NetworkOhtomiRepository
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.example.deliveryapp.model.DeviceInfoRepository
+import com.example.deliveryapp.model.DeviceInfoRepositoryImpl
 import com.example.deliveryapp.model.OhtomiRepository
+import com.example.deliveryapp.model.OhtomiRepositoryImpl
+import com.example.deliveryapp.network.DeviceTokenApiService
 import com.example.deliveryapp.network.OhtomiApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
+import javax.inject.Singleton
 
-interface AppContainer {
-    val ohtomiRepository: OhtomiRepository
-}
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DeviceTokenRetrofit
 
-class OhtomiAppContainer : AppContainer {
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class OhtomiRetrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
     private val deviceTokenUrl = "http://133.17.165.165:8086/"
     private val ohtomiUrl = "https://ohtomi.apps.kyusan-u.ac.jp/"
 
-    private val deviceTokenRetrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(deviceTokenUrl)
-        .build()
-
-    private val ohtomiRetrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(ohtomiUrl)
-        .build()
-
-    private val deviceTokenApiService: OhtomiApiService by lazy {
-        deviceTokenRetrofit.create(OhtomiApiService::class.java)
+    @Provides
+    @Singleton
+    @DeviceTokenRetrofit
+    fun deviceTokenRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(deviceTokenUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 
-    private val ohtomiApiService: OhtomiApiService by lazy {
-        ohtomiRetrofit.create(OhtomiApiService::class.java)
+    @Provides
+    @Singleton
+    @OhtomiRetrofit
+    fun ohtomiRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(ohtomiUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 
-    override val ohtomiRepository: OhtomiRepository by lazy {
-        NetworkOhtomiRepository(deviceTokenApiService, ohtomiApiService)
+    @Provides
+    @Singleton
+    fun provideDeviceTokenApiService(@DeviceTokenRetrofit retrofit: Retrofit): DeviceTokenApiService {
+        return retrofit.create(DeviceTokenApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOhtomiApiService(@OhtomiRetrofit retrofit: Retrofit): OhtomiApiService {
+        return retrofit.create(OhtomiApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocationRepository(
+        deviceTokenApiService: DeviceTokenApiService,
+        ohtomiApiService: OhtomiApiService
+    ): OhtomiRepository {
+        return OhtomiRepositoryImpl(deviceTokenApiService, ohtomiApiService)
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile("device_info") }
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDeviceInfoRepository(
+        @ApplicationContext context: Context,
+        dataStore: DataStore<Preferences>
+    ): DeviceInfoRepository {
+        return DeviceInfoRepositoryImpl(context, dataStore)
     }
 }
